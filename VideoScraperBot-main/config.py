8f419ec281d00c0258784cf4d7ee541d6953d532
jerrywan44 <59520573+jerrywan44@users.noreply.github.com>
2023-09-20 11:27:19 -0700

    Add files via upload

---
+++ VideoScraperBot-main/config.py
@@ -0,0 +1,7 @@
+telegram_token = '6483287271:AAGqq9lxq_JRh0rVE3lViJ9drWb25wWPMUE'
+admin_chat_id = 743299567
+user_chat_id = 743299567
+video_path = '/mp4/'
+audio_path = '/mp3/'
+enable_ftp = 'false'
+ftp_site = 'https://path.to.site/'
